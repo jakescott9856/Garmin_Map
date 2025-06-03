7b939d186e6376b071f95f779a5769e6f91de6a5
Jake Scott <js2591@cam.ac.uk>
2025-06-03 22:31:08 +0100

    overhaul to csv format and line colours

--- graph.py
+++ graph.py
@@ -17,34 +17,44 @@ def main(User):
     # Create a map centered about lat long
     map_center = [52.1936, -2.7216]
     my_map = folium.Map(location=map_center, zoom_start=7)
+    unknown_activity_list = []
+    # Helper function to extract lat/lon from file
+    def extract_latlon(filepath):
+        # Read only the point data (after 3 metadata lines + 1 blank line)
+        return pd.read_csv(filepath, skiprows=1)[['latitude', 'longitude']]
 
     # Plot line for each file except the last modified file
     for fname in CSV_filenames:
-        if fname != last_modified_file:
-            num_current += 1
-            print('Graphing: ' + User + ' ' + fname + ' (' + str(num_current) + '/' + num_files + ')')
-            full_path = os.path.join(CSV_path, fname)
-            points_df = pd.read_csv(full_path)
-            df = points_df[['latitude', 'longitude']]
+        num_current += 1
+        print('Graphing: ' + User + ' ' + fname + ' (' + str(num_current) + '/' + num_files + ')')
+        full_path = os.path.join(CSV_path, fname)
 
+        # Optional: read and display metadata
+        metadata = pd.read_csv(full_path, nrows=1, header=None)
+        activity = metadata.iloc[0,1].lower() if metadata.shape[1] > 1 else ''
+       
+        df = extract_latlon(full_path)
+
+        if 'cycling' in activity:
             line_color = 'blue'
-            
-            folium.PolyLine(
-                locations=df[['latitude', 'longitude']].values,
-                color=line_color,
-            ).add_to(my_map)
+        elif 'sailing' in activity or 'kayaking' in activity or 'boating' in activity:
+            line_color = 'white'
+        elif 'running' in activity or 'walking' in activity or 'hiking' in activity:
+            line_color = 'green'
+        else:
+            line_color = 'black'
+            unknown_activity_list.append(full_path)
+        folium.PolyLine(
+            locations=df.values,
+            color=line_color,
+        ).add_to(my_map)
 
     # Plot line for the last modified file
     full_path = os.path.join(CSV_path, last_modified_file)
-    points_df = pd.read_csv(full_path)
-    df = points_df[['latitude', 'longitude']]
-
-    folium.PolyLine(
-        locations=df[['latitude', 'longitude']].values,
-        color='red',
-    ).add_to(my_map)
+    df = extract_latlon(full_path)
 
     # Save the map to an HTML file
+    print(unknown_activity_list)
     print('')
     print('----- Saving Map -----')
     map_filepath = os.path.join(User_path, str(User) + "_map.html")
