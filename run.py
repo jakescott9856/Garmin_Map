77745ccf44fe220338c219b549717f675e7f5079
Jake Scott <js2591@cam.ac.uk>
2025-09-07 20:34:28 +0100

    SCC

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import parse_fit
 import graph
+from datetime import date
+fdate = date.today().strftime('%d/%m/%Y')
 
 User = 'JS'
 Map_display = 'All'
-Red_modified = '04/07/2030'
+#Red_modified = fdate  
+Red_modified= '01/01/2099'
 
 parse_fit.main(User)
 graph.main(User,Map_display,Red_modified)
